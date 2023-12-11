f19161572310df39c9be33e9432db70ee554e1f4
fortywinters <cui_aylmer@163.com>
2023-12-12 02:01:14 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,15 @@
 
 # 配置nvim
 git clone https://github.com/FortyWinters/Neovim.git ~/.config/nvim
-sh ~/.config/nvim/script/install_ubuntu.sh
+
+sudo apt-get install -y neovim
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip
+unzip FiraCode.zip -d ~/.fonts
+fc-cache -fv
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+rm ~/.config/nvim/plugins/alphy.lua
